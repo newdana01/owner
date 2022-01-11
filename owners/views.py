f2fe76999472ae67e66508a8b6c7462797108c03
newdana01 <newdana01@gmail.com>
2022-01-12 02:45:33 +0900

    get()구현 및 post() 수정

--- owners/views.py
+++ owners/views.py
@@ -14,13 +14,61 @@ class OwnerView(View):
             )
 
         return JsonResponse({'message':'SUCCESS'},status=201)
+        
+    def get(self, request):
+        owners = Owner.objects.all()
+
+        result = []
+        for index, owner in enumerate(owners):
+            result.append(
+                {   
+                    'name': owner.name,
+                    'age': owner.age,
+                    'email': owner.email
+                }
+            )
+
+            if owner.related_dog.all().exists():
+                dog_result = []
+                for dog in owner.related_dog.all():
+                    dog_result.append(
+                        {
+                            'dog_name': dog.name,
+                            'dog_age': dog.age
+                        }
+                    )
+                result[index]['dog'] = dog_result
+                print(index)
+                print(result)
+
+        return JsonResponse({'owners':result}, status=200)
 
 class DogView(View):
     def post(self, request, owners_id):
         data = json.loads(request.body)
 
-        Dog.objects.create(
-            name=data["name"], age=data["age"], owner_id=owners_id
+        try:
+            owner = Owner.objects.get(id=owners_id)
+            Dog.objects.create(
+                name=data["name"], age=data["age"], owner_id=owner.id
+                )
+        except Owner.DoesNotExist:
+            return JsonResponse({'message':'Owner does not exists.'}, status=400)
+        else: 
+            return JsonResponse({'message':'SUCCESS'},status=201)
+
+    def get(self, request, owners_id):
+        dogs = Dog.objects.filter(owner=owners_id)
+
+        result = []
+
+        for dog in dogs:
+            result.append(
+                {
+                    'name': dog.name,
+                    'age': dog.age,
+                    'owner_name': dog.owner.name       
+                }
             )
 
-        return JsonResponse({'message':'SUCCESS'},status=201)
+        return JsonResponse({'dogs': result}, status=200)
