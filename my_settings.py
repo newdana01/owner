773dbb98a42c9a85a29aaade6ce9b543db2a57ef
newdana01 <newdana01@gmail.com>
2022-01-11 16:44:21 +0900

    CREATE: 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'owner',
+		'USER': 'root',
+		'PASSWORD': '1234',
+		'HOST': 'localhost',
+		'PORT': '3306',
+	}
+}
+
+SECRET_KEY = 'django-insecure-cqf5m(bo6wyf0#n6(1ap3110v6^=d&jsjp9qdid6&(p^631n+i'
