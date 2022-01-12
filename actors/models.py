43dca06fa689489a8167ac510dc45785ef35588e
newdana01 <newdana01@gmail.com>
2022-01-12 17:38:43 +0900

    models.py 수정, urlpatterns 등록

--- actors/models.py
+++ actors/models.py
@@ -6,14 +6,14 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
-    movie = models.ManyToManyField('Movie',through='ActorMovie')
+    movie = models.ManyToManyField('movies.Movie',through='ActorMovie')
 
     class Meta:
         db_table = 'actors'
 
 class ActorMovie(models.Model):
     actor = models.ForeignKey('Actor', on_delete=CASCADE)
-    movie = models.ForeignKey('Movie', on_delete=CASCADE)
+    movie = models.ForeignKey('movies.Movie', on_delete=CASCADE)
 
     class Meta:
         db_table = 'actors_movies'
