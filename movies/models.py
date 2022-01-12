6abb42b8a685495f7f828874836244f5896e93a0
newdana01 <newdana01@gmail.com>
2022-01-12 16:21:13 +0900

    Movie 클래스 정의

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    running_time = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'movies'
